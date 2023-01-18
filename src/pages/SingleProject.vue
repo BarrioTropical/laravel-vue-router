96427874ae0b36744f8688baa97caa25df48e0e1
“BarrioTropical” <barrio>
2023-01-18 16:41:29 +0100

    menu + redirect 404
    Signed-off-by: “BarrioTropical” <“barrio>

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -15,8 +15,22 @@
             project: null,
             } 
         }, 
-        method:{
+        methods:{
+            getProjects(){
+                axios.get(`${this.store.apiBaseUrl}/projects/${this.$route.params.slug}`).then((response)=>{
+                    console.log(response.data.results);
+                    if(response.data.success){
+                        this.project = response.data.results;
+                    }else{
+                        console.log(this.$router);
+                        this.$router.push({name: 'not-found'})
+                    }
+                })
+            },
             
+        },
+        mounted(){
+            this.getProjects();
         }
     }
 </script>
